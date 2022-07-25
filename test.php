8b83f940b3470f8811c0edd49ef7cab2c5dfdfea
ian-mlolwa <75843266+Ian-Mlolwa@users.noreply.github.com>
2022-07-25 14:45:37 +0300

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<form action="posttest.php">
-<div class="column3" style="border-style: groove; height: 60px;">
-                            Antenna<input type="checkbox" name="other_equipment[]" value="Antenna">
-                            Radio<input type="checkbox" name="other_equipment[]" value="Radio">
-                            Fiber Router<input type="checkbox" name="other_equipment[]" value="Fiber Router">
-                            PoE Adapter<input type="checkbox" name="other_equipment[]" value="PoE Adapter"><br>
-                    </div>
-                    <input type="submit" value="submit">
-                    </form>
